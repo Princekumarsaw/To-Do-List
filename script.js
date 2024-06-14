c4c6693bd44c6d9a5c3c9570f9ec15595fb099d8
Prince kumar <156781863+Princekumarsaw@users.noreply.github.com>
2024-06-14 23:02:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,97 +1,52 @@
-window.addEventListener('load', () => {
-	const form = document.querySelector("#new-task-form");
-	const input = document.querySelector("#new-task-input");
-	const list_el = document.querySelector("#tasks");
+const todoInput = document.querySelector("#todo-input");
+const todoButton = document.querySelector("#todo-btn");
+const todoBox = document.querySelector(".todos");
 
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
+const addTodo = () => {
+  //add new todo
+  const todoItem = document.createElement("div");
+  todoItem.classList.add("todo-item");
+  todoItem.innerHTML = `<input type="checkbox" class="todo-check" >
+                <p class="todo">${todoInput.value}</p>
+                <span class="edit"><i class="fa-solid fa-pen-to-square fa-xs"></i></span>
+                <span class="delete"><i class="fa-solid fa-trash fa-xs"></i></span>`;
+  todoBox.prepend(todoItem);
+  todoInput.value = "";
 
-		const task = input.value;
-
-		const task_el = document.createElement('div');
-		task_el.classList.add('task');
-
-		const task_content_el = document.createElement('div');
-		task_content_el.classList.add('content');
-
-		task_el.appendChild(task_content_el);
-
-		const task_input_el = document.createElement('input');
-		task_input_el.classList.add('text');
-		task_input_el.type = 'text';
-		task_input_el.value = task;
-		task_input_el.setAttribute('readonly', 'readonly');
-
-		task_content_el.appendChild(task_input_el);
-
-		const task_actions_el = document.createElement('div');
-		task_actions_el.classList.add('actions');
-		
-		const task_edit_el = document.createElement('button');
-		task_edit_el.classList.add('edit');
-		task_edit_el.innerText = 'Edit';
-
-		const task_delete_el = document.createElement('button');
-		task_delete_el.classList.add('delete');
-		task_delete_el.innerText = 'Delete';
-
-		task_actions_el.appendChild(task_edit_el);
-		task_actions_el.appendChild(task_delete_el);
-
-		task_el.appendChild(task_actions_el);
-
-		list_el.appendChild(task_el);
-
-		input.value = '';
-
-		task_edit_el.addEventListener('click', (e) => {
-			if (task_edit_el.innerText.toLowerCase() == "edit") {
-				task_edit_el.innerText = "Save";
-				task_input_el.removeAttribute("readonly");
-				task_input_el.focus();
-			} else {
-				task_edit_el.innerText = "Edit";
-				task_input_el.setAttribute("readonly", "readonly");
-			}
-		});
-
-		task_delete_el.addEventListener('click', (e) => {
-			list_el.removeChild(task_el);
-		});
-	});
-});
+  const editButtons = document.querySelectorAll(".edit");
+  const deleteButtons = document.querySelectorAll(".delete");
+  const checkboxes = document.querySelectorAll(".todo-check");
 
 
-//theme
-let themeToggler = document.querySelector('.theme-toggler');
+  //delete button
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      button.parentElement.remove();
+    });
+  });
 
-themeToggler.onclick = () =>{
+  //edit button
 
-    themeToggler.classList.toggle('active');
+  editButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      todoInput.value = button.previousElementSibling.innerText;
+      button.parentElement.remove();
+    });
+  });
 
-    if(themeToggler.classList.contains('active')){
-        document.body.classList.add('active');
-    }else{
-        document.body.classList.remove('active');
-    }
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("click",()=>{
+        checkbox.nextElementSibling.classList.toggle("mark");
+    });
 
-}
+  });
 
-document.querySelectorAll('.theme-colors .color').forEach(color =>{
-    color.onclick = () => {
-        let background = color.style.background;
-        document.querySelector(':root').style.setProperty('--darker', background);
-    }
-}); 
+};
 
-let theme = document.querySelector('.themes-container');
+todoButton.addEventListener("click", addTodo);
 
-document.querySelector('#theme-open').onclick = () =>{
-    theme.classList.add('active');
-    document.body.style.paddingRight = '350px';
-}
-
-document.querySelector('#theme-close').onclick = () =>{
-    theme.classList.remove('active');
-    document.body.style.paddingRight = '0px';
-}
+document.addEventListener("keydown",(e) => {
+  if(e.key == "Enter"){
+    addTodo();
+  }
+})
